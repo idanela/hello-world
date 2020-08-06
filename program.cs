b5073efb3171567edd1837a7523be701fec40fa4
idanela <58819088+idanela@users.noreply.github.com>
2020-08-06 20:50:12 +0300

    first uploaded c# program
    test uploading file to github

---
+++ program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace firstUploadeToGit
+{
+    public class program
+    {
+         public static void Main()
+        {
+            System.Console.WriteLine("hi there");
+        }
+    }
+}
